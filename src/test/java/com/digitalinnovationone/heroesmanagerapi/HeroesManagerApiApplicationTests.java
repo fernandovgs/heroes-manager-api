304189e9fd99a41cc0a7142c2680a68bddbfd4d2
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-04-04 12:56:37 -0300

    initial commit

---
+++ src/test/java/com/digitalinnovationone/heroesmanagerapi/HeroesManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.heroesmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
