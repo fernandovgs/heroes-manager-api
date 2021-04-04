38bcbd78ff8904302fbed157faf6f613de3c29d9
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-04-04 17:44:26 -0300

    basic dynamodb config

---
+++ src/main/java/com/digitalinnovationone/heroesmanagerapi/config/DynamoConfig.java
@@ -0,0 +1,41 @@
+package com.digitalinnovationone.heroesmanagerapi.config;
+
+import com.amazonaws.auth.*;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDynamoDBRepositories
+public class DynamoConfig {
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
+
+    @Value("${amazon.dynamodb.region}")
+    private String signingRegion;
+
+    @Bean
+    public DynamoDBMapperConfig dynamoDBMapperConfig() {
+        return DynamoDBMapperConfig.DEFAULT;
+    }
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withCredentials(amazonAWSCredentialsProvider()).withEndpointConfiguration(
+                        new EndpointConfiguration(amazonDynamoDBEndpoint, signingRegion)
+                ).build();
+    }
+
+    @Bean("AWSCredentialsProvider")
+    public AWSCredentialsProvider amazonAWSCredentialsProvider() {
+        return new ProfileCredentialsProvider();
+    }
+}
